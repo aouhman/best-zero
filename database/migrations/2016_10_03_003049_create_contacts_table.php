77004c1da91a38ec0a0d861f2b42b4404cdd48a3
aouhman <abdessamad.ouhman@gmail.com>
2016-10-03 00:55:59 +0000

    BDD FILE

---
+++ database/migrations/2016_10_03_003049_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->text('company');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contacts');
+    }
+}
