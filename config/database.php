530359abadec02ea8e331e0fd5a4a0e994888108
aouhman <abdessamad.ouhman@gmail.com>
2017-02-18 23:32:54 +0000

    edit configuration

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'best_zero'),
+            'database' => env('DB_DATABASE', 'best-zero'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
